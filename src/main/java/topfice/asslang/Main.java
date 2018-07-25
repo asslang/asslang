764c1cde7e5ad6c03c5ba720196bcb29e519bb0b
Chris Tran <Chris Tran@DESKTOP-UHGUKU2.mshome.net>
2018-07-25 18:25:54 +0700

    Initialize

---
+++ src/main/java/topfice/asslang/Main.java
@@ -0,0 +1,13 @@
+package topfice.asslang;
+
+/**
+ *
+ * @author Chris Tran
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Asslang version 0.0.1-beta");
+        System.out.println("--------------------------");
+    }
+}
